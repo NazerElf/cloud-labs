446854e7de0130133fef0eacec9807567d8d2276
NazerElf <zlzeds@gmail.com>
2025-12-23 02:17:04 +0200

    test again ci cd

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ authorizations = {
 }
 
 # Initialize Flask-RESTX
-api = Api(app, doc='/api/docs', title='Sensor API', description='Sensor Management API',
+api = Api(app, doc='/api/docs', title='Sensor API', description='Test',
           authorizations=authorizations, security='Bearer Auth')
 
 register_routes(api)
